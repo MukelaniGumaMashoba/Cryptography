3492867058c144ff9d0c4e861e670e64e2748bfb
Mukelani Mashoba <mukelanilastborn@gmail.com>
2026-01-28 19:35:31 +0200

    Initialize salt.js with crypto imports and AES configuration

--- salt.js
+++ salt.js
@@ -1,1 +1,7 @@
 // we need to first import the library and then using symmetric encryption and randombytes for salt
+
+import crypto from 'crypto';
+import { Buffer } from 'buffer';
+
+const ALGORITHM = 'aes-256-cbc';
+const IV_LENGTH = 16; // AES block size
